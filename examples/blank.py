1cee033ef574421da4fabac0f27b648a06ff32a2
Tony Lechner <tony@tony-lechner.com>
2022-03-02 06:40:40 -0600

    Restructure examples directory
    We will no longer be abstracting the entirety of tkinter. Instead, we
    will be a widget. Prep example code with an example window.

--- examples/blank.py
+++ examples/blank.py
@@ -1,8 +1,23 @@
 import sys
+import tkinter
+from tkinter import ttk
+
+
+class ExampleWindow(tkinter.Tk):
+    def __init__(
+        self, width=800, height=600, title="A tcl/tkursed 2D renderer", resizable=False
+    ):
+        super().__init__()
+        self.title(title)
+        self.geometry(f"{width}x{height}")
+        self.resizable(resizable, resizable)
+        self.frame: ttk.Frame = ttk.Frame(self, width=width, height=height)
+        self.frame.pack()
 
 
 def main() -> int:
-    raise NotImplementedError
+    ExampleWindow().mainloop()
+    return 0
 
 
 if __name__ == "__main__":
