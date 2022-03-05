52c2b11b25082c71bf1093332d388214ce164107
Tony Lechner <tony@tony-lechner.com>
2022-03-05 03:47:17 -0600

    Remove unused TypeVar

--- src/tkursed/_state.py
+++ src/tkursed/_state.py
@@ -1,13 +1,16 @@
 import abc
 import dataclasses
-from typing import Any, Callable, TypeVar
-
-T = TypeVar("T")
+from typing import Callable
 
 
 class _TkursedState(abc.ABC):
+    def __post_init__(self):
+        errors = self.validate()
+        if errors:
+            raise ValueError("validation errors", errors)
+
     @abc.abstractmethod
-    def validate(self) -> dict[str, tuple[Any, ...]]:
+    def validate(self) -> dict[str, ValueError]:
         raise NotImplementedError
 
 
@@ -17,15 +20,21 @@ class State(_TkursedState):
     canvas_height: int = 600
     pixel: tuple[int, int, int] = (0, 0, 0)
 
-    def validate(self) -> dict[str, tuple[Any, ...]]:
-        errors: dict[str, tuple[Any, ...]] = {}
+    def validate(self) -> dict[str, ValueError]:
+        errors: dict[str, ValueError] = {}
         if self.canvas_width <= 0:
-            errors["canvas_width"] = ("nonpositive canvas_width", self.canvas_width)
+            errors["canvas_width"] = ValueError(
+                "nonpositive canvas_width", self.canvas_width
+            )
         if self.canvas_height <= 0:
-            errors["canvas_height"] = ("nonpositive canvas_height", self.canvas_height)
+            errors["canvas_height"] = ValueError(
+                "nonpositive canvas_height", self.canvas_height
+            )
 
         if any((v for v in self.pixel if not 0 <= v <= 255)):
-            errors["pixel"] = ("pixel byte out of range 0<=value<=255", self.pixel)
+            errors["pixel"] = ValueError(
+                "pixel byte out of range 0<=value<=255", self.pixel
+            )
 
         return errors
 
