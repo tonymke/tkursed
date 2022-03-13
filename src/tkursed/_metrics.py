bda7184c99f949f8eff9da6101bfe5612a28d33b
Tony Lechner <tony@tony-lechner.com>
2022-03-13 14:21:02 -0500

    Add _metrics docstrings

--- src/tkursed/_metrics.py
+++ src/tkursed/_metrics.py
@@ -6,6 +6,19 @@ from collections.abc import Generator
 def moving_count(
     window_duration_ms: int | float = 1000.0,
 ) -> Generator[int, None, None]:
+    """Generate a moving-window count based on the amount of next() calls.
+
+    Keyword Arguments:
+        window_duration_ms -- int | float: the size of the moving window, in
+                              milliseconds (default: {1000.0})
+
+    Raises:
+        ValueError: nonpositive window_duration_ms
+
+    Yields:
+        The amount of next() calls made in the last window_duration_ms milliseconds as
+        of the time of the invoking next() call.
+    """
     if window_duration_ms <= 0:
         raise ValueError("nonpositive window_duration_ms")
 
